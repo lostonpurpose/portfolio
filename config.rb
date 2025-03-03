89691ddb9c627dbab27fd8c1fa5c3b627a550fad
Will VanderWyden <vanderwyden@gmail.com>
2025-03-03 16:29:58 +0900

    adflk

--- config.rb
+++ config.rb
@@ -1,54 +1,57 @@
-# Activate and configure extensions
-# https://middlemanapp.com/advanced/configuration/#configuring-extensions
+  # Activate and configure extensions
+  # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
-activate :autoprefixer do |prefix|
-  prefix.browsers = "last 2 versions"
-end
+  activate :autoprefixer do |prefix|
+    prefix.browsers = "last 2 versions"
+  end
 
-# activate :sassc
-activate :directory_indexes
-activate :sprockets
-activate :relative_assets #chatgpt change...
+  # activate :sassc
+  activate :directory_indexes
+  activate :sprockets
+  activate :relative_assets
+  set :relative_links, true
+  set :http_prefix, "/portfolio"  # Ensure this matches your GitHub repo name
 
 
-# Layouts
-# https://middlemanapp.com/basics/layouts/
 
-# Per-page layout changes
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
+  # Layouts
+  # https://middlemanapp.com/basics/layouts/
 
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
+  # Per-page layout changes
+  page '/*.xml', layout: false
+  page '/*.json', layout: false
+  page '/*.txt', layout: false
 
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
+  # With alternative layout
+  # page '/path/to/file.html', layout: 'other_layout'
 
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
+  # Proxy pages
+  # https://middlemanapp.com/advanced/dynamic-pages/
 
-# Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
+  # proxy(
+  #   '/this-page-has-no-template.html',
+  #   '/template-file.html',
+  #   locals: {
+  #     which_fake_page: 'Rendering a fake page with a local variable'
+  #   },
+  # )
 
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
+  # Helpers
+  # Methods defined in the helpers block are available in templates
+  # https://middlemanapp.com/basics/helper-methods/
 
-# Build-specific configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
+  # helpers do
+  #   def some_helper
+  #     'Helping'
+  #   end
+  # end
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript, compressor: Terser.new
-# end
+  # Build-specific configuration
+  # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-set :url_root, "https://lostonpurpose.github.io/portfolio"
+  # configure :build do
+  #   activate :minify_css
+  #   activate :minify_javascript, compressor: Terser.new
+  # end
+
+  set :url_root, "https://lostonpurpose.github.io/portfolio"
